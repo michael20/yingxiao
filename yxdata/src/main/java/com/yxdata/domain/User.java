2780171afed449288dae46ce18302fb97d3e3dfd
Gua <332093548@qq.com>
2018-05-29 16:19:19 +0800

    完善User表

--- yxdata/src/main/java/com/yxdata/domain/User.java
+++ yxdata/src/main/java/com/yxdata/domain/User.java
@@ -4,11 +4,26 @@ import lombok.Data;
 
 @Data
 public class User {
-	private int id;
+	
+	//id
+	private Integer id;
+	
+	//客户名称
 	private String name;
+	
+	//联系电话
 	private String tel;
+	
+	//客户地址
 	private String addr;
-	private int type_id;
-	private int del_flag;
+	
+	//用电类型
+	private Integer typeId;
+	
+	//删除标志
+	private Integer delFlag;
+	
+	//备注
 	private String remark;
+	
 }
