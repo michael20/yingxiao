081ef0303d63ad0bfaa1ea8d942598bf47f9211b
Aubrey <676719570@qq.com>
2018-05-30 15:16:51 +0800

    修改过滤条件，首页

---
+++ yxdata/src/main/java/com/yxdata/controller/LoginController.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package com.yxdata.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Administrator
+ * @date 2018年5月30日 下午3:10:17
+ * 
+ */
+@Controller
+public class LoginController {
+	
+	
+	@RequestMapping("/login")
+	public String login(HttpServletRequest req, HttpServletResponse resp) {
+		String name = req.getParameter("name");
+		name = name!=null?name:"";
+		String password = req.getParameter("password");
+		password = password!=null?password:"";
+		if("admin".equals(name) && "admin".equals(password)) {
+			HttpSession session = req.getSession();
+			session.setAttribute("name", name);
+//			登录成功
+			return "index";
+			
+		}
+		req.setAttribute("error", "用户名或密码错误！请重试！");
+		return "redirect:/login.jsp";
+	}
+}
