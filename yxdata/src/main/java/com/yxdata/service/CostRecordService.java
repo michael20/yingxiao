9f67c030680da53c4ee46cdd614bd0b129a9f2fb
michael <michael.20@qq.com>
2018-05-30 09:32:07 +0800

    提交CostRecordService接口及其实现类

---
+++ yxdata/src/main/java/com/yxdata/service/CostRecordService.java
@@ -0,0 +1,18 @@
+package com.yxdata.service;
+
+import com.yxdata.domain.CostRecord;
+
+public interface CostRecordService {
+
+	int deleteByPrimaryKey(Integer id);
+
+    int insert(CostRecord record);
+
+    int insertSelective(CostRecord record);
+
+    CostRecord selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(CostRecord record);
+
+    int updateByPrimaryKey(CostRecord record);
+}
---
+++ yxdata/src/main/java/com/yxdata/service/impl/CostRecordServiceImpl.java
@@ -0,0 +1,60 @@
+package com.yxdata.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yxdata.dao.CostRecordMapper;
+import com.yxdata.domain.CostRecord;
+import com.yxdata.service.CostRecordService;
+
+@Service
+public class CostRecordServiceImpl implements CostRecordService {
+
+	@Autowired
+	private CostRecordMapper costRecordMapper;
+	
+	
+	@Override
+	public int deleteByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		
+		return costRecordMapper.deleteByPrimaryKey(id);
+//		return 0;
+	}
+
+	@Override
+	public int insert(CostRecord record) {
+		// TODO Auto-generated method stub
+		return costRecordMapper.insert(record);
+//		return 0;
+	}
+
+	@Override
+	public int insertSelective(CostRecord record) {
+		// TODO Auto-generated method stub
+		return costRecordMapper.insertSelective(record);
+//		return 0;
+	}
+
+	@Override
+	public CostRecord selectByPrimaryKey(Integer id) {
+		// TODO Auto-generated method stub
+		return costRecordMapper.selectByPrimaryKey(id);
+//		return null;
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(CostRecord record) {
+		// TODO Auto-generated method stub
+		return costRecordMapper.updateByPrimaryKeySelective(record);
+//		return 0;
+	}
+
+	@Override
+	public int updateByPrimaryKey(CostRecord record) {
+		// TODO Auto-generated method stub
+		return costRecordMapper.updateByPrimaryKey(record);
+//		return 0;
+	}
+
+}
